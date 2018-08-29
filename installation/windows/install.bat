618cc8dfa804e50df9e3f1914352628ccf88caa7
Dustin Summers <dustin.summers@fortegollc.com>
2018-08-28 17:30:31 -0700

    Removing old files

--- installation/windows/install.bat
+++
@@ -1,7 +0,0 @@
-echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "\n=-=-=-=-= Uninstalling Previous Versions of IoTHub =-=-=-=-\n"
-pip3 uninstall iothub -y
-echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "\n-=-=-=-= Installing IoTHub =-=-=-=-=-\n"
-pip3 install ../..
-echo "\n-=-=-=-= Finished -=-=-=-=-=-=-=-=-\n"
